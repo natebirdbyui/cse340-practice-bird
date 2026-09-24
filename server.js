af94380882b8b74df40f5efd10f28cebcce0d76d
natebirdbyui <nathan.bird@hotmail.com>
2026-09-23 19:23:03 -0600

    update u1 pt2

--- server.js
+++ server.js
@@ -1,21 +1,86 @@
+/**
+ *  Import libraries
+ */
+
 // Import express using ESM syntax
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+/**
+ * Declare Important Variables
+ */
+// Define the port number the server will listen on
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const PORT = process.env.PORT || 3000;
+const name = process.env.NAME; // <--NEW
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+/**
+ * Setup Express Server
+ */
 // Create an instance of an Express application
 const app = express();
 
-const name = process.env.NAME; // <--NEW
+/**
+ * Configure Express middleware
+ */
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
 
-// Define a route handler for the root URL ('/')
+/**
+ * Global template variables middleware
+ * 
+ * Makes common variables available to all EJS templates without having to pass
+ * them individually from each route handler
+ */
+app.use((req, res, next) => {
+    // Make NODE_ENV available to all templates
+    res.locals.NODE_ENV = NODE_ENV.toLowerCase() || 'production';
+    // Continue to the next middleware or route handler
+    next();
+});
+
+/**
+ * Routes
+ */
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`);
+    const title = 'Welcome Home';
+    res.render('home', { title });
+});
+app.get('/about', (req, res) => {
+    const title = 'About Me';
+    res.render('about', { title });
+});
+app.get('/products', (req, res) => {
+    const title = 'Our Products';
+    res.render('products', { title });
 });
 
-app.get('/new-route', (req, res) => {
-    res.send('This is a new route!');
-});
-// Define the port number the server will listen on
-const PORT = 3000;
-// Start the server and listen on the specified port
+// When in development mode, start a WebSocket server for live reloading
+if (NODE_ENV.includes('dev')) {
+    const ws = await import('ws');
+    try {
+        const wsPort = parseInt(PORT) + 1;
+        const wsServer = new ws.WebSocketServer({ port: wsPort });
+        wsServer.on('listening', () => {
+            console.log(`WebSocket server is running on port ${wsPort}`);
+        });
+        wsServer.on('error', (error) => {
+            console.error('WebSocket server error:', error);
+        });
+    } catch (error) {
+        console.error('Failed to start WebSocket server:', error);
+    }
+}
+
+/**
+ * Start the server and listen on the specified port
+ */
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
